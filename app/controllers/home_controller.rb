e2fd333c88d0c3d9425afdb97f532446ff4dada9
kevine97 <kevinesquivelrojas@hotmail.com>
2020-07-26 16:25:02 -0600

    version 2.0

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   #before_action :authenticate_user!
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
   def index
+    @movies = Movie.joins(:cine).all
   end
 end
